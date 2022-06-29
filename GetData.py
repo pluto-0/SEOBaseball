031c90c31eaef6399d9bf8da30904da7cecedf2c
Joseph Aguiar <aguiarjo@bc.edu>
2022-06-29 21:34:13 +0000

    Style corrections

--- GetData.py
+++ GetData.py
@@ -1,5 +1,9 @@
 import requests
+import json
+import pandas
+import sqlalchemy as db
 from matplotlib import pyplot as plt
+
 
 def is_from_country(id, country):
   url = "http://lookup-service-prod.mlb.com/json/named.player_info.bam?sport_code='mlb'&player_id=" + str(id) +""
@@ -7,6 +11,7 @@ def is_from_country(id, country):
   if response.json()['player_info']['queryResults']['row']['birth_country'].lower() == country.lower():
     return True
   return False
+
 
 def get_teams(year):
   team_ids = []
@@ -22,17 +27,33 @@ def get_teams(year):
       team_ids.append(i['mlb_org_id'])
   return team_ids
 
+
 def get_players(country, year):
   teams = get_teams(year)
   players = []
   for i in teams:
     roster_url = "http://lookup-service-prod.mlb.com/json/named.roster_team_alltime.bam?start_season=" + str(year) + "&end_season=" + str(year) + "&team_id=" + str(i) + ""
     response = requests.get(roster_url)
-    data = response.json()['roster_team_alltime']['queryResults']['row']
+    try:
+      data = response.json()['roster_team_alltime']['queryResults']['row']
+    except:
+      continue
     for j in data:
-      if is_from_country(j['player_id'], country):
-        players.append(j['name_first_last'])
+      try:
+        if is_from_country(j['player_id'], country):
+          players.append(j['name_first_last'])
+      except:
+        continue
   return players
+
+
+def sql_database(data, country):
+  pandas.DataFrame.from_dict(data, orient='index')
+  engine = db.create_engine('sqlite:///' + country + ''.db')
+  dataframe_name.to_sql(country, con=engine, if_exists='replace', index=False)
+  query_result = engine.execute("SELECT * FROM " + country + ";").fetchall()
+  print(pd.DataFrame(query_result))
+
 
 def main():
   print("This program will create a graph of the number of mlb players from a certain country over time")
@@ -42,12 +63,26 @@ def main():
   print("Creating graph, this may take a while.  Press ctrl + C to quit")
 
   num_of_players = []
+  data = {}
   for i in range(start_year, end_year):
-    num_of_players.append(len(get_players(country, i)))
+    print("Processing " + str(i))
+    try:
+      data[str('i')] = get_players(country, i)
+      num_of_players.append(len(data))
+    except:
+      continue
 
+  with open(str(country + str(start_year) + "_" + str(end_year) + ".json"), "w") as json_file:
+    json.dump(data, json_file)
    
   plt.plot(list(range(start_year, end_year)), num_of_players)
-  plt.show()
+  
+  plt.xlabel("Year")
+  plt.ylabel("Players from " + country.capitalize())
+  plt.title("MLB players from " + country.capitalize())
+
+  plt.savefig(country.capitalize() + str(start_year) + "_" + str(end_year)  + ".png")
+
 
 if __name__ == '__main__':
   main()
